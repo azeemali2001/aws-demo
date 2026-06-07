c5183fa61be5ec79bfc5ff57fe21eedf3101cc0d
Azeem Ali <aliazeem7428@gmail.com>
2026-06-07 21:01:35 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from AWS EC2"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running");
+});
